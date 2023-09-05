e5d5a08ccaac488d171e22586e91f0d8d279fafc
Erick El nino <erick.tshimbombo@yahoo.com>
2023-09-05 18:59:57 +0200

    work with small project

---
+++ src/Challenge.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { useState } from "react";
+
+function TextExpander({
+  collapsedNumWords,
+  expandButtonText = "Show less",
+  collapseButtonText,
+  buttonColor,
+  expanded,
+  className,
+  children,
+}) {
+  const [isExpanded, setIsExpanded] = useState(expanded);
+  const buttonStyle = {
+    border: "none",
+    marginLeft: "0.5rem",
+    backgroundColor: "transparent",
+  };
+
+  const handleExpand = () => {
+    setIsExpanded(!isExpanded);
+    console.log(isExpanded);
+  };
+
+  let whattoshow = children.length;
+  const displayText = isExpanded
+    ? children
+    : children.substring(0, whattoshow / 2);
+
+  const linkStyle = {
+    textDecoration: "none",
+    letterSpacing: "1px",
+  };
+  return (
+    <div
+      className={className}
+      style={{ width: "80%", margin: "10px auto" }}
+      className={className}
+    >
+      {displayText}
+      <button type="button" style={buttonStyle} onClick={handleExpand}>
+        <a href="#" style={linkStyle}>
+          {isExpanded ? expandButtonText : "Show more"}
+        </a>
+      </button>
+    </div>
+  );
+}
+
+export const Challenge = () => {
+  return (
+    <div>
+      <TextExpander expanded={true}>
+        Space travel is the ultimate adventure! Imagine soaring past the stars
+        and exploring new worlds. It's the stuff of dreams and science fiction,
+        but believe it or not, space travel is a real thing. Humans and robots
+        are constantly venturing out into the cosmos to uncover its secrets and
+        push the boundaries of what's possible.
+      </TextExpander>
+
+      <TextExpander
+        expanded={false}
+        collapsedNumWords={20}
+        expandButtonText="Show More"
+        collapseButtonText="Show Less"
+        buttonColor="#ff6622"
+      >
+        Space travel requires some seriously amazing technology and
+        collaboration between countries, private companies, and international
+        space organizations. And while it's not always easy (or cheap), the
+        results are out of this world. Think about the first time humans stepped
+        foot on the moon or when rovers were sent to roam around on Mars.
+      </TextExpander>
+
+      <TextExpander expanded={true} className="box">
+        Space missions have given us incredible insights into our universe and
+        have inspired future generations to keep reaching for the stars. Space
+        travel is a pretty cool thing to think about. Who knows what we'll
+        discover next!
+      </TextExpander>
+    </div>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,32 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
-import { StarRating } from "./StarRating";
-import { useState } from "react";
 
-const Starts = ({ color = "blue" }) => {
-  const [MovieRating, SetMovieRating] = useState(0);
-  return (
-    <div>
-      <StarRating color={color} setMovie={SetMovieRating} />
-      <p>This movie was rated {MovieRating} Stars</p>
-    </div>
-  );
-};
+import "./style.css";
+import { Challenge } from "./Challenge";
+// const Starts = ({ color = "blue" }) => {
+//   const [MovieRating, SetMovieRating] = useState(0);
+//   return (
+//     <div>
+//       <StarRating color={color} setMovie={SetMovieRating} />
+//       <p>This movie was rated {MovieRating} Stars</p>
+//     </div>
+//   );
+// };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-
-    <StarRating
+    <Challenge />
+    {/* <StarRating
       maxRating={5}
       size={40}
       message={["Not good", "Bad", "Good", "Amazing", "Excellent"]}
       defaultRating={3}
     />
-    <StarRating maxRating={5} size={24} color={"red"} />
-    <Starts />
+    <StarRating maxRating={5} size={24} color={"red"} /> */}
   </React.StrictMode>
 );
