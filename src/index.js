b4335c3cf91463ed431cfa88688270073953259f
Erick El nino <erick.tshimbombo@yahoo.com>
2023-09-05 02:33:41 +0200

    update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      size={40}
+      message={["Not good", "Bad", "Good", "Amazing", "Excellent"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} size={24} color={"red"} />
   </React.StrictMode>
 );
